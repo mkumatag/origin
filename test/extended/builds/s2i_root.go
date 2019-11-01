95c9e864df7b4b449a8b5f0ad4bf9dc5ad983490
Manjunath A Kumatagi <mkumatag@in.ibm.com>
2019-11-02 00:12:47 +0530

    Fix image name

--- test/extended/builds/s2i_root.go
+++ test/extended/builds/s2i_root.go
@@ -77,7 +77,7 @@ var _ = g.Describe("[Feature:Builds][Conformance] s2i build with a root user ima
 		})
 		o.Expect(err).NotTo(o.HaveOccurred())
 
-		err = oc.Run("new-build").Args("docker.io/openshift/test-build-roots2i~https://github.com/sclorg/nodejs-ex", "--name", "nodejspass").Execute()
+		err = oc.Run("new-build").Args("docker.io/openshifttests/test-build-roots2i~https://github.com/sclorg/nodejs-ex", "--name", "nodejspass").Execute()
 		o.Expect(err).NotTo(o.HaveOccurred())
 
 		err = exutil.WaitForABuild(oc.BuildClient().BuildV1().Builds(oc.Namespace()), "nodejspass-1", nil, nil, nil)
